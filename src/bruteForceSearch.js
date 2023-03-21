57e74566919dcdedd6aa68518d0ea5dc7ea2f115
Samuel Majerus <ladolego@gmail.com>
2023-03-21 12:31:03 -0700

    [bruteForceSearch-] Logic, Test 1 -- Pass.  [Part of Lsn 2 (see link). [This is for 1st prompt, a.k.a the one before Binary Search.]] https://www.learnhowtoprogram.com/computer-science/algorithms/binary-search-algorithm

--- src/bruteForceSearch.js
+++ src/bruteForceSearch.js
@@ -1,12 +1,12 @@
 //Iterate through the array -- beginning with first index -- to search for a stored value which matches that which is passed into the function. If it is found, return the index where the value is located at. If the array doesn't include the value, return -1.   -(Paraphrased from prompt 1 in [Comp Sci Wk 4,] Lsn 2). 
 
 function bruteForceSearch(num, array) {
-  // for (let i = 0; i < array.length; i++) {
-  //   if (array[i] === num) {
-  //     return i;
-  //   }
-  // }
-  // return -1;  //Executes only if the above loop ends without the if-statement triggering.
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      return i;
+    }
+  }
+  return -1;  //Executes only if the above loop ends without the if-statement triggering.
 }
 
 export default bruteForceSearch;
