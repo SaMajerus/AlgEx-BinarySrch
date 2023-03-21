88c95584665cb14aecf66389a207eec7fcb3443e
Samuel Majerus <ladolego@gmail.com>
2023-03-21 12:26:54 -0700

    Initial commit -- adds the rest of the 'initial' files (wrote Soln logic and tests for Prompt 1 before making this repo).

---
+++ __tests__/bruteForceSearch.test.js
@@ -0,0 +1,19 @@
+import bruteForceSearch from '../src/bruteForceSearch.js';
+
+describe('bruteForceSearch', () => {
+
+  //Iterate through the array -- beginning with first index -- to search for a stored value which matches that which is passed into the function. If it is found, return the index where the value is located at. If the array doesn't include the value, return -1.   -(Paraphrased from prompt 1 in [Comp Sci Wk 4,] Lsn 2). 
+  const sortedArray = [3, 9, 14, 34, 45, 56, 63, 72, 88, 101, 132, 151, 168, 192];
+
+  // test('should return the location in the array that contains the specified value', () => {
+  //   expect().toEqual();
+  //   expect().toEqual();
+  //   expect().toEqual();
+  //   expect().toEqual();
+  //   expect().toEqual();
+  // });
+
+  // test('should search for a value that is not stored in the array, and return -1', () => {
+  //   expect().toEqual(-1);
+  // });
+});
