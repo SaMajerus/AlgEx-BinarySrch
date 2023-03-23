bd0f46dedf4eb377d57aafba92792e36d1e4a432
Samuel Majerus <ladolego@gmail.com>
2023-03-23 14:05:47 -0700

    Refactoring done -- reused code from bruteForceSearch test suite.

--- __tests__/binarySearch.test.js
+++ __tests__/binarySearch.test.js
@@ -2,18 +2,13 @@ import binarySearch from '../src/binarySearchAlg.js';
 
 describe('binarySearch', () => {
 
-  // //Iterate through the array -- beginning with first index -- to search for a stored value which matches that which is passed into the function. If it is found, return the index where the value is located at. If the array doesn't include the value, return -1.   -(Paraphrased from prompt 1 in [Comp Sci Wk 4,] Lsn 2). 
-  // const sortedArray = [3, 9, 14, 34, 45, 56, 63, 72, 88, 101, 132, 151, 168, 192];
+  const sortedArray = [3, 9, 14, 34, 45, 56, 63, 72, 88, 101, 132, 151, 168, 192];
 
   // test('should return the location in the array that contains the specified value', () => {
-  //   expect(bruteForceSearch(14, sortedArray)).toEqual(2);
-  //   // expect().toEqual();
-  //   // expect().toEqual();
-  //   // expect().toEqual();
-  //   // expect().toEqual();
+  //   expect(binarySearch(14, sortedArray)).toEqual(2);
   // });
 
   // test('should search for a value that is not stored in the array, and return -1', () => {
-  //   expect(bruteForceSearch(999, sortedArray)).toEqual(-1);
+  //   expect(binarySearch(999, sortedArray)).toEqual(-1);
   // });
 });
